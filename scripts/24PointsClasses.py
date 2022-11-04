3535f7a382b951818c4e0a2afdf428744ee73f36
FriendChicken <78368334+FriendChicken@users.noreply.github.com>
2022-11-04 15:59:30 +0000

    Create 24PointsClasses.py

---
+++ scripts/24PointsClasses.py
@@ -0,0 +1,15 @@
+List_of_Operations=["add","times","power","root","divide","minus"]
+class Algebraic24:
+  def __init__(self, name, inverse):
+    self.name=name
+    self.inverse=inverse
+  def __str__(self):
+    return self.name
+
+class Operations24:
+  def __init__(self, name, inverse):
+    self.name=name
+    self.inverse=inverse
+  def __str__(self):
+    return self.name
+  
