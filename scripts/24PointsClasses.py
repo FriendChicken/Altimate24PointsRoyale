986aa3af91f1938f2e91b617e241eaa04f889494
FriendChicken <pengyuanqi644@gmail.com>
2022-11-10 16:41:58 +0000

    Update 24PointsClasses.py

--- scripts/24PointsClasses.py
+++ scripts/24PointsClasses.py
@@ -1,4 +1,13 @@
 List_of_Operations=["add","times","power","root","divide","minus"]
+class Tree:
+    def __init__(self, left=None,right=None,data=None):
+        self.degree= None
+        self.left = left
+        self.right = right
+        self.data = data
+    def Post_Order(output): #output is a list
+        
+
 class Algebraic24:
   def __init__(self, name, inverse):
     self.name=name
@@ -10,6 +19,13 @@ class Operations24:
   def __init__(self, name, inverse):
     self.name=name
     self.inverse=inverse
+  def __str__(self):
+    return self.name
+
+class Equation24:
+  def __init__(self, eq):
+    self.eq=Tree()
+
   def __str__(self):
     return self.name
   
